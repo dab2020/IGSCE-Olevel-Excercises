b4f5fbb828d278b696e7e7a34b7d29072ce4d8af
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-24 16:18:53 +0500

    Added operators visual basic file

---
+++ Visual Basic/02. Operators/main.vb
@@ -0,0 +1,21 @@
+Module Module1
+
+    Sub Main()
+        Console.WriteLine(23 < 5)
+        Console.WriteLine(23 > 5)
+        Console.WriteLine(23 = 5)
+        Console.WriteLine(23 <> 5)
+        Console.WriteLine(True And False)
+        Console.WriteLine(True Or False)
+        Console.WriteLine(Not False)
+        Console.WriteLine(Not True)
+        Console.WriteLine(Not True)
+        Console.WriteLine(5 + 5)
+        Console.WriteLine(5 - 5)
+        Console.WriteLine(5 ^ 5)
+        Console.WriteLine(25 / 5)
+        Console.WriteLine(28 Mod 5)
+        Console.ReadKey()
+    End Sub
+
+End Module
