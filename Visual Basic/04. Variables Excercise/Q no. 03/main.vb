3d964752b8911a8642d3062389251b6572786a30
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 14:41:00 +0500

    Added the code

---
+++ Visual Basic/04. Variables Excercise/Q no. 03/main.vb
@@ -0,0 +1,11 @@
+Module Module1
+
+    Sub Main()
+        Dim num As Decimal
+        num = -1
+        Console.WriteLine("If true this number is positive and if false the number is negative")
+        Console.WriteLine(num > 0)
+        Console.ReadKey()
+    End Sub
+
+End Module
