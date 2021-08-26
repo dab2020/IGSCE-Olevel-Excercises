a1b2ad80fa2d6eff30d251e448cd8831468e2dd4
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 13:34:59 +0500

    Showed second method

---
+++ Visual Basic/04. Variables Excercise/Q no. 01/2ndmethod.vb
@@ -0,0 +1,14 @@
+Module Module1
+
+    Sub Main()
+        Dim mynum As Integer
+        mynum = 3
+    Console.WriteLine("Second Method")
+        Dim add As Integer
+        add = 7
+        Console.WriteLine(mynum + add)
+        Console.ReadKey()
+    End Sub
+
+End Module
+    
