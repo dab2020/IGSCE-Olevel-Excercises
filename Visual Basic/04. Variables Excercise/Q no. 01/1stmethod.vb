55d9df476e7695d120f03e4428d32f2de92498c5
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 13:33:31 +0500

    Showed first method

---
+++ Visual Basic/04. Variables Excercise/Q no. 01/1stmethod.vb
@@ -0,0 +1,11 @@
+Module Module1
+
+    Sub Main()
+        Dim mynum As Integer
+        mynum = 3
+        Console.WriteLine("First Method of doing it")
+        Console.WriteLine(mynum + 7)
+        Console.ReadKey()
+        End Sub
+
+End Module
