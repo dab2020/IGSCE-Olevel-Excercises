b28c4e5f25cb42ea8dc94e7324c82aea4267077c
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 12:40:34 +0500

    Create main.vb

---
+++ Visual Basic/03. Variables/main.vb
@@ -0,0 +1,12 @@
+Module Module1
+
+    Sub Main()
+        Dim height, width, area As Integer
+        height = 10
+        width = 15
+        area = height * width
+        Console.WriteLine(area)
+        Console.ReadKey()
+    End Sub
+
+End Module
