4415affe760426894513445c3dd05248ae3fcd2e
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 17:29:23 +0500

    Create main.vb

---
+++ Visual Basic/04. Variables Excercise/Q no. 09/main.vb
@@ -0,0 +1,17 @@
+Module Module1
+
+    Sub Main()
+        Dim pay, wage, time As Integer
+        Dim Name, wage1 As String
+        pay = 500
+        time = 40
+        wage = pay * time
+        wage1 = wage
+        Name = "Anwar"
+        Console.WriteLine("Every week " + Name + " is paid:")
+        Console.WriteLine("Rs." + wage1)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
