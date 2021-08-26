476741a443ae1f5a8eca7c1e3ef54d8fc191b801
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 15:36:49 +0500

    Create main.vb

---
+++ Visual Basic/04. Variables Excercise/Q no. 03/main.vb
@@ -0,0 +1,13 @@
+Sub Main()
+        Dim num As Decimal
+        num = -1
+        If num > 0 Then
+            Console.WriteLine("the number is positive")
+            Console.ReadKey()
+        Else
+            Console.WriteLine("The number is negative")
+            Console.ReadKey()
+        End If
+    End Sub
+
+End Module
