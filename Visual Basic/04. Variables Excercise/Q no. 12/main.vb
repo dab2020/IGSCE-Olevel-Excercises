1183b91d2969d244bd64ad7213f0837e98853e01
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 19:33:49 +0500

    Create main.vb

---
+++ Visual Basic/04. Variables Excercise/Q no. 12/main.vb
@@ -0,0 +1,13 @@
+Module Module1
+
+    Sub Main()
+        Dim quotient, remainder, divisor, dividend As Integer
+        quotient = 3
+        divisor = 8
+        remainder = 6
+        dividend = (quotient * divisor) + remainder
+        Console.WriteLine(dividend)
+        Console.ReadKey()
+    End Sub
+
+End Module
