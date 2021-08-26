5369795da94f447f11cd7a2eb99942217f1d78c1
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 14:16:41 +0500

    fixed bug

--- Visual Basic/04. Variables Excercise/Q no. 02/2ndmethod.vb
+++ Visual Basic/04. Variables Excercise/Q no. 02/2ndmethod.vb
@@ -1,11 +1,10 @@
 Module Module1
 
     Sub Main()
-        Dim mynum As Integer
+        Dim mynum, add As Integer
         mynum = 256
-    Console.WriteLine("Second Method")
-        Dim add As Integer
         add = 2
+    Console.WriteLine("Second Method")
         Console.WriteLine(mynum * add)
         Console.ReadKey()
         End Sub
