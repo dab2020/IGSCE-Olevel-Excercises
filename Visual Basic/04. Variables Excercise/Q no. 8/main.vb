ec8283ed9c79f716f5e73bd23ed841e6bd90bc6d
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 17:12:21 +0500

    Create main.vb

---
+++ Visual Basic/04. Variables Excercise/Q no. 8/main.vb
@@ -0,0 +1,14 @@
+Module Module1
+
+    Sub Main()
+        Dim lenght, width, area As Integer
+        Dim area1 As String
+        lenght = 13
+        width = 15
+        area = lenght * width
+        area1 = area
+        Console.WriteLine(area1 + " cm^2")
+        Console.ReadKey()
+    End Sub
+
+End Module
