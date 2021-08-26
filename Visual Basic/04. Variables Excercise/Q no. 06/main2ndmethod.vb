4193532e95f71316cbf70882ae4f22ba15a1cc05
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 16:44:54 +0500

    Added a 2nd method

---
+++ Visual Basic/04. Variables Excercise/Q no. 06/main2ndmethod.vb
@@ -0,0 +1,11 @@
+Module Module1
+
+    Sub Main()
+        Dim signal, output As Boolean
+        signal = True
+        output = Not True
+        Console.WriteLine(output)
+        Console.ReadKey()
+    End Sub
+
+End Module
