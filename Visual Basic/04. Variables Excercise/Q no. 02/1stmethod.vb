1e26fd6be03793ff242778b52676e94b5f29c997
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 14:04:16 +0500

    Create 1stmethod.vb

---
+++ Visual Basic/04. Variables Excercise/Q no. 02/1stmethod.vb
@@ -0,0 +1,10 @@
+Module Module1
+
+    Sub Main()
+        Dim mynum As Integer
+        mynum = 256
+        Console.WriteLine("First Method of doing it")
+        Console.WriteLine(mynum * 2)
+    End Sub
+
+End Module
