3a80dc47598a8328db72bd32d3b76efe133b4a39
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 17:03:33 +0500

    Create main.vb

---
+++ Visual Basic/04. Variables Excercise/Q no. 07/main.vb
@@ -0,0 +1,13 @@
+Module Module1
+
+    Sub Main()
+        Dim boo1, boo2 As Boolean
+        boo1 = True
+        boo2 = False
+        Console.WriteLine(boo1 And boo2)
+        Console.WriteLine(boo1 Or boo2)
+        Console.WriteLine(boo1 Xor boo2)
+        Console.ReadKey()
+    End Sub
+
+End Module
