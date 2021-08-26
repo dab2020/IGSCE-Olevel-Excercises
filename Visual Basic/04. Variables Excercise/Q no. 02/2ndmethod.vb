ba3ac2949d891e4c11d12b6dc9fc741a4f48e84d
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 14:05:19 +0500

    Showed Second method

---
+++ Visual Basic/04. Variables Excercise/Q no. 02/2ndmethod.vb
@@ -0,0 +1,12 @@
+Module Module1
+
+    Sub Main()
+        Dim mynum As Integer
+        mynum = 256
+    Console.WriteLine("Second Method")
+        Dim add As Integer
+        add = 2
+        Console.WriteLine(mynum * add)
+        End Sub
+
+End Module
