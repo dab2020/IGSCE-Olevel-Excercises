82ec6842af172ef0e4e1ac1c6eca7365a192e054
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 11:57:03 +0500

    Create main.py

---
+++ python/02. Operators/main.py
@@ -0,0 +1,11 @@
+print(5+2)
+print(5-2)
+print(5*2)
+print(25/5)
+print(25 % 2)
+if 5 > 2:
+  print("Five is greater than two!")
+if 1 < 2:
+  print("One is lesser than two!")
+if 5 != 2:
+  print("Five is not equal to two")
