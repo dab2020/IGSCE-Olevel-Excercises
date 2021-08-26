a294504859ae380872258f3902d2227049bb1988
dab2020 <79893903+dab2020@users.noreply.github.com>
2021-08-26 16:26:36 +0500

    Added main .vb file

---
+++ Visual Basic/04. Variables Excercise/Q no. 5/main.vb
@@ -0,0 +1,12 @@
+Module Module1
+
+    Sub Main()
+        Dim word1, word2, word3 As String
+        word1 = "My Name is "
+        word2 = "Dayyan Abdullah"
+        word3 = word1 + word2
+        Console.WriteLine(word3)
+        Console.ReadKey()
+    End Sub
+
+End Module
